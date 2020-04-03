85d546a23351db63a3272300b5974dc32b5b3acb
Sébastien <srouillon94@gmail.com>
2020-04-03 10:00:56 +0200

    Début du bot

---
+++ index.js
@@ -0,0 +1,5 @@
+const Discord = require('discord.js');
+const { Client } = require('discord.js');
+const client = new Discord.Client();
+
+client.login("NTUzMjIzMzU1MjA1OTQzMzI0.D2K-uQ.XL8ZkNc-AZqqTBUooZZRoHF6s70");
